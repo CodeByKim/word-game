03ce49265431f9a47335d8e3bfbd2cb47019a0d3
cookapps-grkim <grkim@cookapps.com>
2020-02-21 16:09:17 +0900

    first commit

---
+++ Assets/WordConnect/Scripts/Editor/WordConnectMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace WordConnect
+{
+	public class WordConnectMenu : MonoBehaviour
+	{
+		[MenuItem("Window/Word Connect Level Builder")]
+		public static void OpenLevelBuilderWindow()
+		{
+			EditorWindow.GetWindow<LevelBuilderWindow>("Level Builder");
+		}
+	}
+}
